6b97c0d880938778c52b38faf183fd6e6a87d6a7
Nathan Batson <nathanbatson@Nathan-Batsons-Mac-mini.local>
2012-06-21 18:10:03 -0400

    added couter_cache for easy sorting on relationships

--- app/models/forem/post.rb
+++ app/models/forem/post.rb
@@ -14,7 +14,7 @@ module Forem
     # Used in the moderation tools partial
     attr_accessor :moderation_option
 
-    belongs_to :topic
+    belongs_to :topic, :counter_cache => true
     belongs_to :user, :class_name => Forem.user_class.to_s
     belongs_to :reply_to, :class_name => "Post"
 
--- app/models/forem/view.rb
+++ app/models/forem/view.rb
@@ -1,6 +1,6 @@
 module Forem
   class View < ActiveRecord::Base
-    belongs_to :topic
+    belongs_to :topic, :counter_cache => true
     belongs_to :user, :class_name => Forem.user_class.to_s
 
     validates :topic_id, :presence => true
