6382640c11cf8f6b635b104db4915f24c9f8e26a
Eivind Hagen <eivind@sibblingz.com>
2012-07-05 10:44:30 -0700

    Fixes crash bug related to cancan and s3 gem. The new code is evaluated later, so it works better.

--- app/controllers/forem/categories_controller.rb
+++ app/controllers/forem/categories_controller.rb
@@ -1,9 +1,10 @@
 module Forem
   class CategoriesController < ApplicationController
-    load_and_authorize_resource
+    # load_and_authorize_resource
 
     def show
       @category = Category.find(params[:id])
+      authorize! :read, @category
     end
   end
 end
